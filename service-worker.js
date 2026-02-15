df209cf86476b0d45c32577f5aa5510d5e8a9202
Crewlobbymadgaon <combinedcrewlobbymadgaon@gmail.com>
2026-02-15 07:45:38 +0530

    Implement service worker with caching strategy

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("duty-cache-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
